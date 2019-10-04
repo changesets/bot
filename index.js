6ad266afcd38affda533a8a3045974d5d72d38b3
mitchellhamilton <mitchell@hamil.town>
2019-10-04 11:30:01 +1000

    Try another thing

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ module.exports = app => {
   app.on(["pull_request.opened", "pull_request.synchronize"], async context => {
     const params = context.issue();
 
-    console.log(context.payload);
+    console.log(JSON.stringify(context.payload));
 
     const commentId = await getCommentId(context, params);
     const hasChangeset = await getChangesetId(context, params);
