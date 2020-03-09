3e73eabfc68d0076440f466884c639180665c526
mitchellhamilton <mitchell@hamil.town>
2020-03-09 15:55:52 +1100

    Change entry to js

---
+++ now.js
@@ -0,0 +1,16 @@
+// inlined from https://unpkg.com/@chadfawcett/probot-serverless-now@1.0.0/index.js
+const { createProbot } = require("probot");
+const { findPrivateKey } = require("probot/lib/private-key");
+const appFn = require("./");
+
+const options = {
+  id: process.env.APP_ID,
+  secret: process.env.WEBHOOK_SECRET,
+  cert: findPrivateKey()
+};
+
+const probot = createProbot(options);
+
+probot.load(appFn);
+
+module.exports = probot.server;
